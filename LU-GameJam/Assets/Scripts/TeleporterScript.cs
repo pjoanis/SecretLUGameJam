538483e5a40bb7848e9e981752454dc4e574b2fd
Patrick Joanis <pjoanis@lakeheadu.ca>
2017-05-29 21:18:14 -0400

    Added a block to load a new scene (from main).
    New scene is empty, but will add to it soon.

---
+++ LU-GameJam/Assets/Scripts/TeleporterScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleporterScript : MonoBehaviour {
+    private bool _isPlayerOverlapping = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_isPlayerOverlapping)
+        {
+            // if key pressed && player overlapping, load the level
+            SceneManager.LoadScene("GameJam");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {            
+            _isPlayerOverlapping = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _isPlayerOverlapping = false;
+        }
+    }
+}
